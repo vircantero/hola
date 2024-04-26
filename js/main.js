4c934eef5c6487256c1402a0a60416f9be2306f9
vircantero <virgican2@gmail.com>
2024-04-26 00:22:54 -0300

    desafio 2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nombreProducto = "Perrito"
+let precioUnitario = 1600
+let cantidadDeseada = prompt("Ingrese la cantidad deseada")
+
+let costoFinal = precioUnitario * cantidadDeseada
+
+console.log(costoFinal);
+
+alert(costoFinal);
